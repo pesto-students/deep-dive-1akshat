56aea4c94d0acb8a21185e440a7b2bfac987fe1c
akki <akshat@shopinsync.com>
2020-04-23 22:06:02 +0530

    throw error for empty string, nomeclature fixes, code made more readable

--- js-exercises/abbreviate-string/abbreviateString.js
+++ js-exercises/abbreviate-string/abbreviateString.js
@@ -1,10 +1,21 @@
 function abbreviateString(str) {
-  if (typeof (str) !== 'string') { throw Error(`Expected string, got ${typeof str}`); }
+  if (typeof (str) !== 'string') {
+    throw Error(`Expected string, got ${typeof str}`);
+  }
+
+  if (str.length < 1) {
+    throw Error('Got empty string.');
+  }
+
+  if (str.split(' ').length === 1) {
+    return str[0].toUpperCase() + str.substring(1);
+  }
 
   const stringArray = str.split(' ');
-  const startString = stringArray[0];
-  const endString = stringArray[stringArray.length - 1];
-  return `${startString} ${endString[0].toUpperCase()}.`;
+  const firstWord = stringArray[0];
+  const lastWord = stringArray[stringArray.length - 1];
+  return `${firstWord} ${lastWord[0].toUpperCase()}.`;
 }
+
 
 export { abbreviateString };
