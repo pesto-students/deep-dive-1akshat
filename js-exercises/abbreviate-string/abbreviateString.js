f7caf3833cd4827ec26203c1ef0e562c69069f8b
akki <akshat@shopinsync.com>
2020-04-23 17:38:56 +0530

    resolve review changes for abbreviate-string solution

--- js-exercises/abbreviate-string/abbreviateString.js
+++ js-exercises/abbreviate-string/abbreviateString.js
@@ -1,10 +1,10 @@
-import { isString } from 'util';
-
 function abbreviateString(str) {
-  if (!isString(str)) throw Error(`Expected string, got ${typeof str}`);
+  if (typeof (str) !== 'string') { throw Error(`Expected string, got ${typeof str}`); }
 
   const stringArray = str.split(' ');
-  return `${stringArray[0]} ${stringArray[stringArray.length - 1][0].toUpperCase()}.`;
+  const startString = stringArray[0];
+  const endString = stringArray[stringArray.length - 1];
+  return `${startString} ${endString[0].toUpperCase()}.`;
 }
 
 export { abbreviateString };
