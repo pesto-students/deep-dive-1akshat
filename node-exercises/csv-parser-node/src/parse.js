b710d9dd00908966a2173fe202a5cdf5ba570421
ajaykumar-mittakola <ajaykumar.mittakola@bankopen.co>
2020-05-07 09:25:02 +0530

    Import csvtojson and consts

--- node-exercises/csv-parser-node/src/parse.js
+++ node-exercises/csv-parser-node/src/parse.js
@@ -1,32 +1,37 @@
 const fs = require('fs');
+const csvtojson = require('./csvtojson');
 
 const FILE_PATH = './files/file.csv';
 const results = [];
 let finalObj = [];
-const DELIMETERS = [',', ':', ' ', '   ', '|', '^', '*', '#', '@', '!', '.', '<', '>', '{', '}', '\t'];
+const DELIMETERS = require('./consts');
 
 
 // Main Function
-const csvParse = (file) => {
-    let readerStream = fs.createReadStream(FILE_PATH);
+const csvParse = (srcFilePath, destinationFilePath) => {
+    // let readerStream = fs.createReadStream(FILE_PATH, 'utf8');
 
-    readerStream.setEncoding('utf8');
+    // readerStream.setEncoding('utf8');
 
-    readerStream.on('readable', () => {
-        console.log()
-    })
+    // readerStream.on('readable', () => {
+    //     console.log()
+    // })
 
-    readerStream.on('data', (chunk) => {
-        results.push(chunk)
-    })
+    // readerStream.on('data', (chunk) => {
+    //     results.push(chunk)
+    // })
 
-    readerStream.on('end', () => {
-        parseCSVToJSON();
-    })
+    // readerStream.on('end', () => {
+    //     parseCSVToJSON();
+    // })
 
-    readerStream.on('error', () => {
-        console.log(err.stack);
-    })
+    // readerStream.on('error', () => {
+    //     console.log(err.stack);
+    // })
+    
+    if(srcFilePath.includes('.csv')) {
+        csvtojson(srcFilePath, destinationFilePath);
+    }
 }
 
-csvParse(FILE_PATH);
+csvParse(FILE_PATH, "./files/file.json");
