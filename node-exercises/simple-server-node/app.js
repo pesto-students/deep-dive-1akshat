d692e8fd0a66292926c2406c2ecd258050880bbd
akki <akshat@shopinsync.com>
2020-05-13 02:31:30 +0530

    Get call valida/ invalid
    add separate routes file to add all the routes, logging valid/ invalid on the basis of the url GET request by the user handle

--- node-exercises/simple-server-node/app.js
+++ node-exercises/simple-server-node/app.js
@@ -1,9 +1,36 @@
 const http = require('http');
+const routes = require('./routes');
 
-const server = http.createServer((req, res) => {
-    res.write('Server is Up and Running');
+const proxy = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    // res.write(req.url);
     res.end();
 })
 
-server.listen(3000);
+proxy.on('request', (req, res) => {
+    if (req.method === 'GET') {
+        const path = req.url;
+        const host = req.headers.host;
+        const urlPartsObj = new URL(path, `http://${host}`);
+        const valid = validateRoute(urlPartsObj);
+        if (valid) {
+            console.log('200 OK');
+        } else {
+            console.log('Not Found. The requested URL was not found on the server. If you entered the URL manually please check your spelling and try again.');
+        }
+    }
+})
+
+const validateRout = (urlPartsObj) => {
+    const validGetRoutes = routes.GET;
+    if (validGetRoutes.includes(urlPartsObj.pathname)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+
+proxy.listen(3000);
 
---
+++ node-exercises/simple-server-node/routes.js
@@ -0,0 +1,14 @@
+// This file will contain all the routes with their methods as the key and the route-string (/ (prefix)) as value
+
+const routes = {
+    GET: [
+        '/home',
+        '/blog'
+    ],
+    POST: [
+        '/login',
+        'signup'
+    ]
+}
+
+module.exports = routes;
