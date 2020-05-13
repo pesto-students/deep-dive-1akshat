196b6d52c7f7c33e3f18d972b0f63eeee14731de
akki <akshat@shopinsync.com>
2020-05-13 22:21:30 +0530

    route handling message: show on view

--- node-exercises/simple-server-node/app.js
+++ node-exercises/simple-server-node/app.js
@@ -3,25 +3,23 @@ const routes = require('./routes');
 
 const proxy = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    // res.write(req.url);
-    res.end();
-})
-
-proxy.on('request', (req, res) => {
     if (req.method === 'GET') {
         const path = req.url;
         const host = req.headers.host;
         const urlPartsObj = new URL(path, `http://${host}`);
-        const valid = validateRoute(urlPartsObj);
+        const valid = validateRoutePath(urlPartsObj);
         if (valid) {
-            console.log('200 OK');
+            res.write('200 OK');
+            res.end();
         } else {
-            console.log('Not Found. The requested URL was not found on the server. If you entered the URL manually please check your spelling and try again.');
+            res.write('Not Found. The requested URL was not found on the server. If you entered the URL manually please check your spelling and try again.');
+            res.end();
         }
     }
 })
 
-const validateRout = (urlPartsObj) => {
+
+const validateRoutePath = (urlPartsObj) => {
     const validGetRoutes = routes.GET;
     if (validGetRoutes.includes(urlPartsObj.pathname)) {
         return true;
@@ -29,8 +27,6 @@ const validateRout = (urlPartsObj) => {
         return false;
     }
 }
-
-
 
 proxy.listen(3000);
 
