81f21fae118e66e06a9c02c21d6fc4661c587bcd
yogi <yogeshwar607@gmail.com>
2020-05-15 20:44:38 +0530

    Add middleware handling

--- node-exercises/simple-server-node/app.js
+++ node-exercises/simple-server-node/app.js
@@ -1,7 +1,56 @@
 // buddy
 
-const Buddy = require('./server');
+const testController = require('./controller/test')
+
+const Buddy = require('./lib/server');
 const routes = require('./routes');
 
-app = new Buddy(routes);
-app.start(8000);
+const config = {
+  httpsOptions:{
+  }
+}
+
+const buddyServer = new Buddy(config);
+
+const bodyMiddleware = ()=>{
+  console.log("bodyMiddleware")
+}
+
+const parser = ()=>{
+  console.log("parser")
+}
+
+
+buddyServer.route({
+  method: 'GET', 
+  path: '/test',
+  middlewares:[bodyMiddleware],
+  handler: testController
+})
+
+buddyServer.route({
+  method: 'post',
+  path: '/test/user/:id',
+  middlewares:[bodyMiddleware],
+  handler: testController
+})
+
+buddyServer.route({
+  method: 'put',
+  path: '/test',
+  middlewares:[bodyMiddleware],
+  handler: testController
+})
+
+
+buddyServer.route({
+  method: 'post',
+  path: '/user/',
+  middlewares:[bodyMiddleware],
+  handler: testController
+})
+
+buddyServer.addMiddleware([parser])
+
+
+buddyServer.start(8000);
