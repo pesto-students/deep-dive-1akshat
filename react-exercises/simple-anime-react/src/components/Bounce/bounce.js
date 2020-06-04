90cff1590b3fa7367a017a65794a3bdc0f8f7131
akki <akshat@shopinsync.com>
2020-06-04 15:45:22 +0530

    add cascade prop for bounce

--- react-exercises/simple-anime-react/src/components/Bounce/bounce.js
+++ react-exercises/simple-anime-react/src/components/Bounce/bounce.js
@@ -2,17 +2,38 @@ import React from 'react';
 import './style.css';
 
 const Bounce = (props) => {
-  const delay = props.duration !== undefined ? props.delay : 1000;
-  const duration = props.duration !== undefined ? props.duration : 3000;
+  const delay = props.duration !== undefined ? Number(props.delay) : 1000;
+  const duration = props.duration !== undefined ? Number(props.duration) : 3000;
+  const cascade = props.cascade !== undefined ? props.cascade : false;
+  let childAnimationDelay = 0;
 
   return (
     <>
-      <div className="bounce" style={{
-        animation: `bounce ${duration}ms`,
-        animationDelay: `${delay}ms`
-      }}>
-        {props.children}
-      </div>
+      {cascade === false ?
+        <div className="bounce" style={{
+          animation: `bounce ${duration}ms`,
+          animationDelay: `${delay}ms`
+        }}>
+          {props.children}
+        </div>
+        :
+        <div>
+          {
+            props.children.map((child, key) => {
+              // 2000 is hardcoded for now
+              childAnimationDelay += 2000;
+              return (
+                <div className="bounce" key={key} style={{
+                  animation: `bounce ${duration + childAnimationDelay}ms`,
+                  animationDelay: `${delay}ms`
+                }}>
+                  {child}
+                </div>
+              )
+            })
+          }
+        </div>
+      }
     </>
   )
 }
