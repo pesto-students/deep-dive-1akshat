b408218c21759e4993ff5daaf177b7f1bd5d5046
akki <akshat@shopinsync.com>
2020-06-06 19:12:57 +0530

    reverted the app view

--- react-exercises/simple-anime-react/src/App.js
+++ react-exercises/simple-anime-react/src/App.js
@@ -1,61 +1,54 @@
 import React from 'react';
 import './App.css';
 import { Bounce, Fade, Pulse, Slide } from './components/index';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Alert } from 'reactstrap';
-
 
 const App = () => {
   return (
     <>
       <h1 style={{ textAlign: 'center' }}>Simple Anime Library</h1>
-
-      <div className="row">
-        <div className="col-md-3">
-          <Bounce animationIterationCount='Infinite'>
-            <img style={{ height: '300px' }} src="https://lh3.googleusercontent.com/proxy/XcRijctCnv9piNAXFEmqcpAmXi0XFDR6ONXIDmG8oNC9DUGzmQP1vVg_rISrBR8uFm0OtTMbadoctWY1xPEeH32wzrkuvUzhZlTOKAhY-OhTdj22_X3KKCwzkmmPxLGHSscxLZdg" alt="ball" />
-          </Bounce>
-        </div>
-
-        <div className="col-md-3" style={{ marginTop: "5%" }} >
-          <Fade cascade={true} delay="500" duration="2000">
-            <Alert color="primary">This is a primary alert — check it out!</Alert>
-            <Alert color="secondary">This is a secondary alert — check it out!</Alert>
-            <Alert color="success">This is a success alert — check it out!</Alert>
-            <Alert color="danger">This is a danger alert — check it out!</Alert>
-            <Alert color="warning">This is a warning alert — check it out!</Alert>
-            <Alert color="info">This is a info alert — check it out!</Alert>
-            <Alert color="light">This is a light alert — check it out!</Alert>
-            <Alert color="dark">This is a dark alert — check it out!</Alert>
-          </Fade>
-        </div>
-
-        <div className="col-md-3" style={{ marginTop: "5%" }} >
-          <Fade duration="2000" delay="500" cascade={true} method="fadeOut">
-            <Alert color="primary">This is a primary alert — check it out!</Alert>
-            <Alert color="secondary">This is a secondary alert — check it out!</Alert>
-            <Alert color="success">This is a success alert — check it out!</Alert>
-            <Alert color="danger">This is a danger alert — check it out!</Alert>
-            <Alert color="warning">This is a warning alert — check it out!</Alert>
-            <Alert color="info">This is a info alert — check it out!</Alert>
-            <Alert color="light">This is a light alert — check it out!</Alert>
-            <Alert color="dark">This is a dark alert — check it out!</Alert>
-          </Fade>
-        </div>
-
-
-        <div className="col-md-3" style={{ marginTop: "5%" }} animationIterationCount='Infinite'>
-          <Pulse duration="400" delay="300" className="parent">
-            <img src="https://icons.iconarchive.com/icons/iconsmind/outline/512/Sound-Wave-icon.png" alt="sound wave" style={{ width: '400px' }} />
-          </Pulse>
-        </div>
-
-        <div className="col-md-3" style={{ marginTop: "5%" }} animationIterationCount='Infinite'>
-          <Slide duration="5000" delay="1000" className="parent" method="up">
-            <div className="block-blue">
-              <p>Slide up</p>
+      <div className="container">
+        <div className="wrap">
+          <Bounce duration="5000" delay="500" animationIterationCount="Infinite">
+            <div className="block-red">
+              <p>Bounce</p>
             </div>
-          </Slide>
+          </Bounce>
+
+          <Fade duration="2000" delay="500" cascade={true} damping="1.5" method="fadeIn" className="animate">
+            <div className="block-grey">
+              <p>Fade In</p>
+            </div>
+            <div className="block-grey">
+              <p>Fade In</p>
+            </div>
+            <div className="block-grey">
+              <p>Fade In</p>
+            </div>
+          </Fade>
+
+          <Fade duration="2000" delay="500" cascade={true} damping="1" method="fadeOut">
+            <div className="block-orange">
+              <p>Fade Out</p>
+            </div>
+            <div className="block-orange">
+              <p>Fade Out</p>
+            </div>
+            <div className="block-orange">
+              <p>Fade Out</p>
+            </div>
+          </Fade>
+
+          <Pulse duration="2000" delay="500" className="parent">
+            <div className="block-cadetblue">
+              <p>Pulse</p>
+            </div>
+          </Pulse>
+
+          {/* <Slide duration="2000" delay="500" className="parent">
+            <div className="block-cadetblue">
+              <p>Slide</p>
+            </div>
+          </Slide> */}
         </div>
       </div>
     </>
