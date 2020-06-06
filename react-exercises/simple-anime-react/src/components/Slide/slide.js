a53bab00e0ccd4183b1a46fceceef4ecc0cf125b
akki <akshat@shopinsync.com>
2020-06-06 19:30:36 +0530

    clean the codebase

--- react-exercises/simple-anime-react/src/components/Slide/slide.js
+++ react-exercises/simple-anime-react/src/components/Slide/slide.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './style.css';
 
 const Slide = (props) => {
-  const delay = props.duration ? Number(props.delay) : 1000;
-  const duration = props.duration ? Number(props.duration) : 3000;
-  const userClass = props.className;
-  const method = props.method ? props.method : 'up';
-  const propStyle = props.style ? props.style : {};
+  // const delay = props.duration ? Number(props.delay) : 1000;
+  // const duration = props.duration ? Number(props.duration) : 3000;
+  // const userClass = props.className;
+  // const method = props.method ? props.method : 'up';
+  // const propStyle = props.style ? props.style : {};
 
   return (
     <>
